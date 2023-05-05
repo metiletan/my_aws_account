90225bbf3b0c03976c0e2b8cf32bb9db052b4506
Anna Strukova <metiletan@gmail.com>
2023-05-05 19:47:46 +0200

    create bucket for tf state

---
+++ state_bucket.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "metil-terraform-state"
+
+  tags = {
+    Name        = "metil-terraform-state"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"  
+    }
+    bucket_key_enabled = true
+  }
+}
